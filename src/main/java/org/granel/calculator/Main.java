cbcc7a22d142c7b369aab2d666b75894459d3b7e
RaulGB88 <raul.granel@gmail.com>
2023-10-03 20:51:38 +0200

    v.1.0.1
    Init Swing Calculator project

---
+++ src/main/java/org/granel/calculator/Main.java
@@ -0,0 +1,17 @@
+package org.granel.calculator;
+
+import org.granel.calculator.app.App;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        // Crear aplicación
+        App app = new App();
+
+        // Lanzar aplicación
+        app.launchApp();
+    }
+
+}
